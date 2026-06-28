7e91342c28fe01dbf1362fe4f1eb8ad260ab6ebb
AndresVilla2008 <andresvillamorales2008@gmail.com>
2026-06-27 22:49:11 -0500

    fix: se corrigio la redireccion al regsitro en el apartado de politicas

--- src/app/features/home/pages/policies/policies.component.ts
+++ src/app/features/home/pages/policies/policies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-policies',
@@ -7,15 +8,26 @@ import { Location } from '@angular/common';
   styleUrls: ['./policies.component.scss']
 })
 export class PoliciesComponent implements OnInit {
+  returnUrl = '/';
 
-  constructor(private location: Location) {}
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     // Desplazar al inicio de la página cuando cargue
     window.scrollTo(0, 0);
+    this.returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
   }
 
   goBack(): void {
+    if (this.returnUrl !== '/') {
+      this.router.navigateByUrl(this.returnUrl);
+      return;
+    }
+
     this.location.back();
   }
 
